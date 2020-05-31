32706bd67cc261d01e39dcd3f711d884fddd18d2
chanisch01 <64133300+chanisch01@users.noreply.github.com>
2020-05-31 17:39:23 +0200

    Änderungen

---
+++ Aufgabe05/Bowls/data.js
@@ -0,0 +1,42 @@
+"use strict";
+//Interface
+let bowl1 = {
+    bild: "bowl1.jpg",
+    name: "Der Snack zwischendurch",
+    beschreibung: "Bowl mit Kiwi und Mango-Mus",
+    preis: "6,99€"
+};
+let bowl2 = {
+    bild: "bowl2.jpg",
+    name: "Gesunder Start in den Tag",
+    beschreibung: "Pudding mit Chia-Samen&Bananen",
+    preis: "5,99"
+};
+let bowl3 = {
+    bild: "bowl3.jpg",
+    name: "Der Sattmacher",
+    beschreibung: "Leckeres Oatmeal mit Rosinen",
+    preis: "6,99€"
+};
+let bowl4 = {
+    bild: "bowl4.jpg",
+    name: "Der Klassiker",
+    beschreibung: "Tofu mit leckerem Gemüse",
+    preis: "8,99€"
+};
+let bowl5 = {
+    bild: "bowl5.jpg",
+    name: "Deftiges Abendessen",
+    beschreibung: "Hackfleisch-Bowl mit Käse und Ei",
+    preis: "9,99€"
+};
+let bowl6 = {
+    bild: "bowl6.jpg",
+    name: "Nährstoff-Bombe",
+    beschreibung: "Süßkartoffeln mit Gemüse und Dip",
+    preis: "9,99€"
+};
+//Array
+let bowls = [bowl1, bowl2, bowl3, bowl4, bowl5, bowl6];
+//# sourceMappingURL=data.js.map
+//# sourceMappingURL=data.js.map
