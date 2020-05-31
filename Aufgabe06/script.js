0dc444205e74ff8310b0157fc3ed8ed757a1d834
chanisch01 <64133300+chanisch01@users.noreply.github.com>
2020-05-31 16:27:25 +0200

    Änderungen

--- Aufgabe06/script.js
+++ Aufgabe06/script.js
@@ -1,58 +1,41 @@
 "use strict";
 var Bowls;
 (function (Bowls) {
-
-
     for (let i = 0; i < bowls.length; i++) {
-
-
         //Überschrift
         let artikelUeberschrift = document.createElement("h2");
         artikelUeberschrift.innerHTML = "Bowls";
         artikelUeberschrift.setAttribute("id", "Überschrift");
-        
         let artikelBowls = document.createElement("h2");
         artikelBowls.innerHTML = "Bowls";
-        artikelBowls.setAttribute("id", "Überschrift");
-        
-    
-
         //div Container für Bowls
         let divBowls = document.createElement("div");
         divBowls.setAttribute("class", "bowls");
-
         //Bild
         let imgBowls = document.createElement("img");
         imgBowls.setAttribute("src", bowls[i].bild);
         imgBowls.setAttribute("alt", "bowls");
-
         //Name des Artikels
         let artikelName = document.createElement("h3");
         artikelName.innerHTML = bowls[i].name;
-
         //Beschreibung des Artikel
         let artikelBeschreibung = document.createElement("h3");
         artikelBeschreibung.innerHTML = bowls[i].beschreibung;
-
         //Preis 
         let bowlsPreis = document.createElement("p");
         bowlsPreis.innerHTML = bowls[i].preis;
-
         //Button "Kaufen"
         let buttonKaufen = document.createElement("button");
         buttonKaufen.innerHTML = "Kaufen";
-
         // Tags werden als child zu div Container hinzugefügt
         divBowls.appendChild(imgBowls);
-        divBowls.appendChild(artikelBowls);
+        divBowls.appendChild(artikelName);
         divBowls.appendChild(artikelBeschreibung);
         divBowls.appendChild(bowlsPreis);
         divBowls.appendChild(buttonKaufen);
-
         //Überschrift und Container hinzufügen
-        document.getElementById("liste")?.appendChild(artikel);
         document.getElementById("liste")?.appendChild(artikelBowls);
+        document.getElementById("liste")?.appendChild(divBowls);
     }
-    
-})(Bowls|| (Bowls = {}));
+})(Bowls || (Bowls = {}));
 //# sourceMappingURL=script.js.map
