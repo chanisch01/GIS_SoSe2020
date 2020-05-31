f18f37aadc61ae19a7e40cdec19ef538ef7eaa2e
chanisch01 <64133300+chanisch01@users.noreply.github.com>
2020-05-31 17:15:09 +0200

    Änderungen

--- Aufgabe05/Zubehör/script.js
+++ Aufgabe05/Zubehör/script.js
@@ -2,55 +2,43 @@
 var Zubehör;
 (function (Zubehör) {
     for (let i = 0; i < zubehör.length; i++) {
-
-
         //Überschrift
         let artikelZubehör = document.createElement("h2");
         artikelZubehör.innerHTML = "Zubehör";
-        artikelZubehör.setAttribute("id", "Überschrift");
-
         if (i == 0) {
             artikelZubehör.innerHTML = "Zubehör";
         }
         else {
             artikelZubehör.innerHTML = " ";
         }
-
         //div Container für Zubehör
         let divZubehör = document.createElement("div");
         divZubehör.setAttribute("class", "zubehör");
-
         //Bild
         let imgZubehör = document.createElement("img");
         imgZubehör.setAttribute("src", zubehör[i].bild);
         imgZubehör.setAttribute("alt", "zubehör");
-
         //Name des Artikels
         let artikelName = document.createElement("h3");
         artikelName.innerHTML = zubehör[i].name;
-
         //Beschreibung des Artikel
         let artikelBeschreibung = document.createElement("h3");
         artikelBeschreibung.innerHTML = zubehör[i].beschreibung;
-
         //Preis 
         let zubehörPreis = document.createElement("p");
         zubehörPreis.innerHTML = zubehör[i].preis;
-
         //Button "Kaufen"
         let buttonKaufen = document.createElement("button");
         buttonKaufen.innerHTML = "Kaufen";
-
         // Tags werden als child zu div Container hinzugefügt
         divZubehör.appendChild(imgZubehör);
-        divZubehör.appendChild(artikelZubehör);
+        divZubehör.appendChild(artikelName);
         divZubehör.appendChild(artikelBeschreibung);
         divZubehör.appendChild(zubehörPreis);
         divZubehör.appendChild(buttonKaufen);
-
         //Überschrift und Container hinzufügen
-        document.getElementById("liste")?.appendChild(artikel);
         document.getElementById("liste")?.appendChild(artikelZubehör);
+        document.getElementById("liste")?.appendChild(divZubehör);
     }
 })(Zubehör || (Zubehör = {}));
 //# sourceMappingURL=script.js.map
