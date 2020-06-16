6a0392c147c97d1a354d8ef43ca4a744c5964e91
chanisch01 <64133300+chanisch01@users.noreply.github.com>
2020-06-16 17:52:46 +0200

    Änderungen

--- Aufgabe07/script.js
+++ Aufgabe07/script.js
@@ -19,90 +19,90 @@ var Aufgabe07;
         angebote = await response.json();
         console.log("End");
         artikelErzeugen();
-    }
-    function saveInLocalStorage(_inputArticle) {
-        let itemString = JSON.stringify(_inputArticle);
-        let key = "" + _inputArticle.name;
-        localStorage.setItem(key, itemString);
-        console.log(localStorage);
-    }
-    //Produkte einschleifen
-    function artikelErzeugen() {
-        for (let i = 0; i < angebote.length; i++) {
-            //Div
-            let newDiv = document.createElement("div");
-            newDiv.id = "produkt" + i;
-            document.getElementById("bowls")?.appendChild(newDiv);
-            //Bild
-            let imgAngebote = document.createElement("img");
-            imgAngebote.src = angebote[i].bild;
-            document.getElementById("produkt" + i)?.appendChild(imgAngebote);
-            //Name anlegen 
-            let nameAngebote = document.createElement("p");
-            nameAngebote.innerHTML = angebote[i].name;
-            document.getElementById("produkt" + i)?.appendChild(nameAngebote);
-            let preisAngebote = document.createElement("p");
-            preisAngebote.innerHTML = angebote[i].preis + "€";
-            document.getElementById("produkt" + i)?.appendChild(preisAngebote);
-            //Beschreibung anlegen 
-            let beschreibungAngebote = document.createElement("p");
-            beschreibungAngebote.innerHTML = angebote[i].beschreibung;
-            document.getElementById("produkt" + i)?.appendChild(beschreibungAngebote);
-            //Button
-            let kaufen = document.createElement("button");
-            kaufen.innerHTML = "In den Warenkorb";
-            document.getElementById("produkt" + i)?.appendChild(kaufen);
-            kaufen.addEventListener("click", handleWarenkorb.bind(angebote[i]));
-            kaufen.setAttribute("preis", angebote[i].preis.toString());
-            switch (angebote[i].kategorien) {
-                case "bowls":
-                    let getContainerBowls = document.getElementById("bowls");
-                    getContainerBowls.appendChild(newDiv);
-                    break;
-                case "zubehoer":
-                    let getContainerZubehoer = document.getElementById("zubehoer");
-                    getContainerZubehoer.appendChild(newDiv);
-                    break;
-                default:
-                    break;
-            }
+        function saveInLocalStorage(_inputArticle) {
+            let itemString = JSON.stringify(_inputArticle);
+            let key = "" + _inputArticle.name;
+            localStorage.setItem(key, itemString);
+            console.log(localStorage);
         }
-        //Teilaufgabe 1:
-        function handleWarenkorb(_event) {
-            produktZähler++;
-            console.log(produktZähler);
-            saveInLocalStorage(this);
-            preisBerechnen += parseFloat(_event.target?.getAttribute("preis"));
-            console.log(preisBerechnen.toFixed(2));
-            if (produktZähler == 1) {
-                document.getElementById("wagenkorbZaehler")?.appendChild(anzahlAnzeigen);
+        //Produkte einschleifen
+        function artikelErzeugen() {
+            for (let i = 0; i < angebote.length; i++) {
+                //Div
+                let newDiv = document.createElement("div");
+                newDiv.id = "produkt" + i;
+                document.getElementById("bowls")?.appendChild(newDiv);
+                //Bild
+                let imgAngebote = document.createElement("img");
+                imgAngebote.src = angebote[i].bild;
+                document.getElementById("produkt" + i)?.appendChild(imgAngebote);
+                //Name anlegen 
+                let nameAngebote = document.createElement("p");
+                nameAngebote.innerHTML = angebote[i].name;
+                document.getElementById("produkt" + i)?.appendChild(nameAngebote);
+                let preisAngebote = document.createElement("p");
+                preisAngebote.innerHTML = angebote[i].preis + "€";
+                document.getElementById("produkt" + i)?.appendChild(preisAngebote);
+                //Beschreibung anlegen 
+                let beschreibungAngebote = document.createElement("p");
+                beschreibungAngebote.innerHTML = angebote[i].beschreibung;
+                document.getElementById("produkt" + i)?.appendChild(beschreibungAngebote);
+                //Button
+                let kaufen = document.createElement("button");
+                kaufen.innerHTML = "In den Warenkorb";
+                document.getElementById("produkt" + i)?.appendChild(kaufen);
+                kaufen.addEventListener("click", handleWarenkorb.bind(angebote[i]));
+                kaufen.setAttribute("preis", angebote[i].preis.toString());
+                switch (angebote[i].kategorien) {
+                    case "bowls":
+                        let getContainerBowls = document.getElementById("bowls");
+                        getContainerBowls.appendChild(newDiv);
+                        break;
+                    case "zubehoer":
+                        let getContainerZubehoer = document.getElementById("zubehoer");
+                        getContainerZubehoer.appendChild(newDiv);
+                        break;
+                    default:
+                        break;
+                }
             }
-            anzahlAnzeigen.innerHTML = "" + produktZähler;
-            document.getElementById("anzahlAnzeigen")?.appendChild(zahlAnzeigen);
+            //Teilaufgabe 1:
+            function handleWarenkorb(_event) {
+                produktZähler++;
+                console.log(produktZähler);
+                saveInLocalStorage(this);
+                preisBerechnen += parseFloat(_event.target?.getAttribute("preis"));
+                console.log(preisBerechnen.toFixed(2));
+                if (produktZähler == 1) {
+                    document.getElementById("wagenkorbZaehler")?.appendChild(anzahlAnzeigen);
+                }
+                anzahlAnzeigen.innerHTML = "" + produktZähler;
+                document.getElementById("anzahlAnzeigen")?.appendChild(zahlAnzeigen);
+            }
+            //Ein-/Ausblenden der Produkte
+            function handleCategoryClick(_click) {
+                switch (this.getAttribute("id")) {
+                    case "bowlsButton":
+                        bowls();
+                        break;
+                    case "zubehoerButton":
+                        zubehoer();
+                        break;
+                }
+                function bowls() {
+                    document.getElementById("bowls").style.display = "inline-grid";
+                    document.getElementById("zubehoer").style.display = "none";
+                }
+                function zubehoer() {
+                    document.getElementById("zubehoer").style.display = "inline-grid";
+                    document.getElementById("bowls").style.display = "none";
+                }
+            }
+            let bowlsAnzeigen = document.querySelector("#bowls");
+            bowlsAnzeigen.addEventListener("click", handleCategoryClick.bind(bowlsAnzeigen));
+            let zubehoerAnzeigen = document.querySelector("#zubehoer");
+            zubehoerAnzeigen.addEventListener("click", handleCategoryClick.bind(zubehoerAnzeigen));
         }
-        //Ein-/Ausblenden der Produkte
-        function handleCategoryClick(_click) {
-            switch (this.getAttribute("id")) {
-                case "bowlsButton":
-                    bowls();
-                    break;
-                case "zubehoerButton":
-                    zubehoer();
-                    break;
-            }
-            function bowls() {
-                document.getElementById("bowls").style.display = "inline-grid";
-                document.getElementById("zubehoer").style.display = "none";
-            }
-            function zubehoer() {
-                document.getElementById("zubehoer").style.display = "inline-grid";
-                document.getElementById("bowls").style.display = "none";
-            }
-        }
-        let bowlsAnzeigen = document.querySelector("#bowls");
-        bowlsAnzeigen.addEventListener("click", handleCategoryClick.bind(bowlsAnzeigen));
-        let zubehoerAnzeigen = document.querySelector("#zubehoer");
-        zubehoerAnzeigen.addEventListener("click", handleCategoryClick.bind(zubehoerAnzeigen));
     }
 })(Aufgabe07 || (Aufgabe07 = {}));
 //# sourceMappingURL=script.js.map
--- Aufgabe07/script.ts
+++ Aufgabe07/script.ts
@@ -33,121 +33,121 @@ namespace Aufgabe07 {
         angebote = await response.json();
         console.log("End");
         artikelErzeugen();
-    }
 
-    function saveInLocalStorage(_inputArticle: Angebote): void {
-        let itemString: string = JSON.stringify(_inputArticle);
-        let key: string = "" + _inputArticle.name;
+        function saveInLocalStorage(_inputArticle: Angebote): void {
+            let itemString: string = JSON.stringify(_inputArticle);
+            let key: string = "" + _inputArticle.name;
 
-        localStorage.setItem(key, itemString);
-        console.log(localStorage);
-    }
-
-    //Produkte einschleifen
-    function artikelErzeugen(): void {
-        for (let i: number = 0; i < angebote.length; i++) {
-
-            //Div
-            let newDiv: HTMLDivElement = document.createElement("div");
-            newDiv.id = "produkt" + i;
-            document.getElementById("bowls")?.appendChild(newDiv);
-
-            //Bild
-            let imgAngebote: HTMLImageElement = document.createElement("img");
-            imgAngebote.src = angebote[i].bild;
-            document.getElementById("produkt" + i)?.appendChild(imgAngebote);
-
-            //Name anlegen 
-            let nameAngebote: HTMLParagraphElement = document.createElement("p");
-            nameAngebote.innerHTML = angebote[i].name;
-            document.getElementById("produkt" + i)?.appendChild(nameAngebote);
-
-            let preisAngebote: HTMLParagraphElement = document.createElement("p");
-            preisAngebote.innerHTML = angebote[i].preis + "€";
-            document.getElementById("produkt" + i)?.appendChild(preisAngebote);
-
-            //Beschreibung anlegen 
-            let beschreibungAngebote: HTMLParagraphElement = document.createElement("p");
-            beschreibungAngebote.innerHTML = angebote[i].beschreibung;
-            document.getElementById("produkt" + i)?.appendChild(beschreibungAngebote);
-
-            //Button
-
-            let kaufen: HTMLButtonElement = document.createElement("button");
-            kaufen.innerHTML = "In den Warenkorb";
-            document.getElementById("produkt" + i)?.appendChild(kaufen);
-            kaufen.addEventListener("click", handleWarenkorb.bind(angebote[i]));
-            kaufen.setAttribute("preis", angebote[i].preis.toString());
-
-
-            switch (angebote[i].kategorien) {
-                case "bowls":
-                    let getContainerBowls: HTMLElement = document.getElementById("bowls")!;
-                    getContainerBowls.appendChild(newDiv);
-                    break;
-
-                case "zubehoer":
-                    let getContainerZubehoer: HTMLElement = document.getElementById("zubehoer")!;
-                    getContainerZubehoer.appendChild(newDiv);
-                    break;
-                default:
-                    break;
-
-            }
+            localStorage.setItem(key, itemString);
+            console.log(localStorage);
         }
 
-        //Teilaufgabe 1:
+        //Produkte einschleifen
+        function artikelErzeugen(): void {
+            for (let i: number = 0; i < angebote.length; i++) {
 
-        function handleWarenkorb(this: Angebote, _event: Event): void {
+                //Div
+                let newDiv: HTMLDivElement = document.createElement("div");
+                newDiv.id = "produkt" + i;
+                document.getElementById("bowls")?.appendChild(newDiv);
 
-            produktZähler++;
-            console.log(produktZähler);
+                //Bild
+                let imgAngebote: HTMLImageElement = document.createElement("img");
+                imgAngebote.src = angebote[i].bild;
+                document.getElementById("produkt" + i)?.appendChild(imgAngebote);
 
-            saveInLocalStorage(this);
-            preisBerechnen += parseFloat((<HTMLButtonElement>_event.target)?.getAttribute("preis")!);
-            console.log(preisBerechnen.toFixed(2));
+                //Name anlegen 
+                let nameAngebote: HTMLParagraphElement = document.createElement("p");
+                nameAngebote.innerHTML = angebote[i].name;
+                document.getElementById("produkt" + i)?.appendChild(nameAngebote);
+
+                let preisAngebote: HTMLParagraphElement = document.createElement("p");
+                preisAngebote.innerHTML = angebote[i].preis + "€";
+                document.getElementById("produkt" + i)?.appendChild(preisAngebote);
+
+                //Beschreibung anlegen 
+                let beschreibungAngebote: HTMLParagraphElement = document.createElement("p");
+                beschreibungAngebote.innerHTML = angebote[i].beschreibung;
+                document.getElementById("produkt" + i)?.appendChild(beschreibungAngebote);
+
+                //Button
+
+                let kaufen: HTMLButtonElement = document.createElement("button");
+                kaufen.innerHTML = "In den Warenkorb";
+                document.getElementById("produkt" + i)?.appendChild(kaufen);
+                kaufen.addEventListener("click", handleWarenkorb.bind(angebote[i]));
+                kaufen.setAttribute("preis", angebote[i].preis.toString());
 
 
-            if (produktZähler == 1) {
-                document.getElementById("wagenkorbZaehler")?.appendChild(anzahlAnzeigen);
+                switch (angebote[i].kategorien) {
+                    case "bowls":
+                        let getContainerBowls: HTMLElement = document.getElementById("bowls")!;
+                        getContainerBowls.appendChild(newDiv);
+                        break;
+
+                    case "zubehoer":
+                        let getContainerZubehoer: HTMLElement = document.getElementById("zubehoer")!;
+                        getContainerZubehoer.appendChild(newDiv);
+                        break;
+                    default:
+                        break;
+
+                }
             }
-            anzahlAnzeigen.innerHTML = "" + produktZähler;
-            document.getElementById("anzahlAnzeigen")?.appendChild(zahlAnzeigen);
+
+            //Teilaufgabe 1:
+
+            function handleWarenkorb(this: Angebote, _event: Event): void {
+
+                produktZähler++;
+                console.log(produktZähler);
+
+                saveInLocalStorage(this);
+                preisBerechnen += parseFloat((<HTMLButtonElement>_event.target)?.getAttribute("preis")!);
+                console.log(preisBerechnen.toFixed(2));
+
+
+                if (produktZähler == 1) {
+                    document.getElementById("wagenkorbZaehler")?.appendChild(anzahlAnzeigen);
+                }
+                anzahlAnzeigen.innerHTML = "" + produktZähler;
+                document.getElementById("anzahlAnzeigen")?.appendChild(zahlAnzeigen);
+
+            }
+
+
+            //Ein-/Ausblenden der Produkte
+
+            function handleCategoryClick(this: HTMLDivElement, _click: MouseEvent): void {
+
+                switch (this.getAttribute("id")) {
+                    case "bowlsButton":
+                        bowls();
+                        break;
+                    case "zubehoerButton":
+                        zubehoer();
+                        break;
+                }
+
+                function bowls(): void {
+                    (<HTMLElement>document.getElementById("bowls")).style.display = "inline-grid";
+                    (<HTMLElement>document.getElementById("zubehoer")).style.display = "none";
+
+                }
+
+                function zubehoer(): void {
+                    (<HTMLElement>document.getElementById("zubehoer")).style.display = "inline-grid";
+                    (<HTMLElement>document.getElementById("bowls")).style.display = "none";
+
+                }
+
+            }
+            let bowlsAnzeigen: HTMLDivElement = <HTMLDivElement>document.querySelector("#bowls");
+            bowlsAnzeigen.addEventListener("click", handleCategoryClick.bind(bowlsAnzeigen));
+
+            let zubehoerAnzeigen: HTMLDivElement = <HTMLDivElement>document.querySelector("#zubehoer");
+            zubehoerAnzeigen.addEventListener("click", handleCategoryClick.bind(zubehoerAnzeigen));
 
         }
-
-
-        //Ein-/Ausblenden der Produkte
-
-        function handleCategoryClick(this: HTMLDivElement, _click: MouseEvent): void {
-
-            switch (this.getAttribute("id")) {
-                case "bowlsButton":
-                    bowls();
-                    break;
-                case "zubehoerButton":
-                    zubehoer();
-                    break;
-            }
-
-            function bowls(): void {
-                (<HTMLElement>document.getElementById("bowls")).style.display = "inline-grid";
-                (<HTMLElement>document.getElementById("zubehoer")).style.display = "none";
-
-            }
-
-            function zubehoer(): void {
-                (<HTMLElement>document.getElementById("zubehoer")).style.display = "inline-grid";
-                (<HTMLElement>document.getElementById("bowls")).style.display = "none";
-
-            }
-
-        }
-        let bowlsAnzeigen: HTMLDivElement = <HTMLDivElement>document.querySelector("#bowls");
-        bowlsAnzeigen.addEventListener("click", handleCategoryClick.bind(bowlsAnzeigen));
-
-        let zubehoerAnzeigen: HTMLDivElement = <HTMLDivElement>document.querySelector("#zubehoer");
-        zubehoerAnzeigen.addEventListener("click", handleCategoryClick.bind(zubehoerAnzeigen));
-
     }
 }
