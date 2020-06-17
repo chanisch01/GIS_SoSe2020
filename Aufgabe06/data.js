d5401cade1780e3deb6df5d0761864ec1bc6d46d
chanisch01 <64133300+chanisch01@users.noreply.github.com>
2020-06-17 17:55:37 +0200

    Änderungen

---
+++ Aufgabe06/data.js
@@ -0,0 +1,92 @@
+"use strict";
+var Aufgabe06;
+(function (Aufgabe06) {
+    //Bowls
+    let bowl1 = {
+        bild: "bowl1.jpg",
+        name: "Der Snack zwischendurch",
+        beschreibung: "Bowl mit Kiwi und Mango-Mus",
+        preis: 6.99,
+        kategorie: "bowl"
+    };
+    let bowl2 = {
+        bild: "bowl2.jpg",
+        name: "Gesunder Start in den Tag",
+        beschreibung: "Pudding mit Chia Samen",
+        preis: 5.99,
+        kategorie: "bowl"
+    };
+    let bowl3 = {
+        bild: "bowl3.jpg",
+        name: "Der Sattmacher",
+        beschreibung: "Leckeres Oatmeal mit Rosinen",
+        preis: 7.99,
+        kategorie: "bowl"
+    };
+    let bowl4 = {
+        bild: "bowl4.jpg",
+        name: "Der Klassiker",
+        beschreibung: "Tofu mit leckerem Gemüse",
+        preis: 8.99,
+        kategorie: "bowl"
+    };
+    let bowl5 = {
+        bild: "bowl5.jpg",
+        name: "Deftiges Abendessen",
+        beschreibung: "Hacklfeisch Bowl mit Käse und Ei",
+        preis: 9.99,
+        kategorie: "bowl"
+    };
+    let bowl6 = {
+        bild: "bowl6.jpg",
+        name: "Nährstoff-Bombe",
+        beschreibung: "Süßkartoffeln mit Gemüse und Dip",
+        preis: 10.99,
+        kategorie: "bowl"
+    };
+    //Zubehoer
+    let zubehoer1 = {
+        bild: "zubehoer1.jpg",
+        name: "Smoothie Mixer",
+        beschreibung: "Ein Muss in der Küche",
+        preis: 20.99,
+        kategorie: "zubehoer"
+    };
+    let zubehoer2 = {
+        bild: "zubehoer2.jpg",
+        name: "Messer",
+        beschreibung: "Ein Muss in der Küche",
+        preis: 23.99,
+        kategorie: "zubehoer"
+    };
+    let zubehoer3 = {
+        bild: "zubehoer3.jpg",
+        name: "6 Einweggläser",
+        beschreibung: "Ein Muss in der Küche",
+        preis: 6.99,
+        kategorie: "zubehoer"
+    };
+    let zubehoer4 = {
+        bild: "zubehoer4.jpg",
+        name: "4 Kochlöffel",
+        beschreibung: "Ein Muss in der Küche",
+        preis: 2.99,
+        kategorie: "zubehoer"
+    };
+    let zubehoer5 = {
+        bild: "zubehoer5.jpg",
+        name: "Schüssel",
+        beschreibung: "Ein Muss in der Küche",
+        preis: 5.99,
+        kategorie: "zubehoer"
+    };
+    let zubehoer6 = {
+        bild: "zubehoer6.jpg",
+        name: "2 Gläser",
+        beschreibung: "Ein Muss in der Küche",
+        preis: 5.99,
+        kategorie: "zubehoer"
+    };
+    Aufgabe06.angebote = [bowl1, bowl2, bowl3, bowl4, bowl5, bowl6, zubehoer1, zubehoer2, zubehoer3, zubehoer4, zubehoer5, zubehoer6];
+})(Aufgabe06 || (Aufgabe06 = {}));
+//# sourceMappingURL=data.js.map
