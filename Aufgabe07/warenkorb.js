4e8b73673dafe540f709beb7ce0c06f2600921b3
chanisch01 <64133300+chanisch01@users.noreply.github.com>
2020-06-17 17:46:12 +0200

    Änderungen

---
+++ Aufgabe07/warenkorb.js
@@ -0,0 +1,70 @@
+"use strict";
+var Aufgabe07;
+(function (Aufgabe07) {
+    window.addEventListener("load", init);
+    let contentDiv;
+    let pGesamtpreis;
+    let gesamtpreis;
+    function init(_event) {
+        contentDiv = document.querySelector(".warenliste");
+        pGesamtpreis = document.querySelector("#total");
+        pGesamtpreis.addEventListener("click", removeAll);
+        document.getElementById("warenkorbWert")?.appendChild(pGesamtpreis);
+        console.log(localStorage);
+        update();
+    }
+    function update() {
+        contentDiv.innerHTML = "";
+        gesamtpreis = 0;
+        for (let i = 0; i < localStorage.length; i++) {
+            let key = localStorage.key(i);
+            let articlesJSON = localStorage.getItem(key);
+            let item = JSON.parse(articlesJSON);
+            gesamtpreis += item.preis;
+            createDynamicContent(item);
+        }
+        setGesamtpreis();
+    }
+    function createDynamicContent(_inputArticle) {
+        //Div erstellen
+        let newDiv = document.createElement("div");
+        contentDiv.appendChild(newDiv);
+        newDiv.id = _inputArticle.name;
+        console.log(newDiv.id);
+        //IMG IN DIV PACKEN
+        let imgAngebote = document.createElement("img");
+        imgAngebote.src = _inputArticle.img;
+        document.getElementById("produkt")?.appendChild(imgAngebote);
+        console.log(imgAngebote);
+        //NAME
+        let name = document.createElement("p");
+        name.innerHTML = _inputArticle.name;
+        newDiv.appendChild(name);
+        //PREIS
+        let price = document.createElement("p");
+        price.innerHTML = "" + _inputArticle.preis;
+        newDiv.setAttribute("preis", price.innerHTML);
+        newDiv.appendChild(price);
+        //BESCHREIBUNG
+        let beschreibung = document.createElement("p");
+        beschreibung.innerHTML = _inputArticle.beschreibung;
+        newDiv.appendChild(beschreibung);
+        //BUTTON
+        let kaufen = document.createElement("button");
+        kaufen.innerHTML = "Löschen";
+        newDiv.appendChild(kaufen);
+        kaufen.addEventListener("click", handleRemoveArticle.bind(_inputArticle));
+    }
+    function handleRemoveArticle(_event) {
+        localStorage.removeItem(this.name);
+        update();
+    }
+    function setGesamtpreis() {
+        pGesamtpreis.innerHTML = "" + gesamtpreis.toFixed(2);
+    }
+    function removeAll(_event) {
+        localStorage.clear();
+        update();
+    }
+})(Aufgabe07 || (Aufgabe07 = {}));
+//# sourceMappingURL=warenkorb.js.map
