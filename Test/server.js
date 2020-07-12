4630896e42c93c6f7da894aaf24d280684b243a3
chanisch01 <64133300+chanisch01@users.noreply.github.com>
2020-07-12 14:09:37 +0200

    .

---
+++ Test/server.js
@@ -0,0 +1,44 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Aufgabe11 = void 0;
+const Http = require("http");
+const Url = require("url");
+const Mongo = require("mongodb");
+var Aufgabe11;
+(function (Aufgabe11) {
+    let collection;
+    let databaseUrl = "mongodb+srv://new_user:hallo@chanida.jbyiv.mongodb.net/Aufgabe?retryWrites=true&w=majority";
+    console.log("Starting server");
+    let port = Number(process.env.PORT);
+    if (!port)
+        port = 8100;
+    connectToDatabase(databaseUrl);
+    let server = Http.createServer();
+    server.addListener("request", handleRequest);
+    server.addListener("listening", handleListen);
+    server.listen(port);
+    async function connectToDatabase(_url) {
+        let options = { useNewUrlParser: true, useUnifiedTopology: true };
+        let mongoClient = new Mongo.MongoClient(_url, options);
+        await mongoClient.connect();
+        collection = mongoClient.db("Test").collection("Students");
+        console.log("Database connection ", collection != undefined);
+    }
+    function handleListen() {
+        console.log("Listening");
+    }
+    async function handleRequest(_request, _response) {
+        _response.setHeader("content-type", "text/html; charset=utf-8");
+        _response.setHeader("Access-Control-Allow-Origin", "*");
+        if (_request.url) {
+            let url = Url.parse(_request.url, true);
+            if (url.pathname == "/senden")
+                collection.insertOne(url.query);
+            else if (url.pathname == "/holen") {
+                _response.write(JSON.stringify(await collection.find().toArray()));
+            }
+        }
+        _response.end();
+    }
+})(Aufgabe11 = exports.Aufgabe11 || (exports.Aufgabe11 = {}));
+//# sourceMappingURL=server.js.map
