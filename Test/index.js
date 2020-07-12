433a3efe336e45012739b60b7ae1712e05976f8d
chanisch01 <64133300+chanisch01@users.noreply.github.com>
2020-07-12 14:16:20 +0200

    .

--- Test/index.js
+++ Test/index.js
@@ -9,18 +9,20 @@ var Aufgabe11;
     let htmltext = document.getElementById("text");
     async function buttonclicksenden() {
         formData = new FormData(document.forms[0]);
-        let url = "https://csgis2020.herokuapp.com";
+        //let url: string = "http://localhost:8100";
+        let url = "https://gissommersemester2020.herokuapp.com";
         let query = new URLSearchParams(formData);
         url = url + "/senden" + "?" + query.toString();
         await fetch(url);
     }
     async function buttonclickholen() {
-        let url = "https://csgis2020.herokuapp.com";
+        let url = "https://gissommersemester2020.herokuapp.com";
+        //let url: string = "http://localhost:8100";
         url = url + "/holen";
+        //BUGS gefixt
         let response = await fetch(url);
         let responseString = await response.text();
         htmltext.innerHTML = responseString;
         console.log("holen");
     }
 })(Aufgabe11 || (Aufgabe11 = {}));
-//# sourceMappingURL=index.js.map
--- Test/server.js
+++ Test/server.js
@@ -6,13 +6,17 @@ const Url = require("url");
 const Mongo = require("mongodb");
 var Aufgabe11;
 (function (Aufgabe11) {
-    let collection;
-    let databaseUrl = "mongodb+srv://new_user:hallo@chanida.jbyiv.mongodb.net/Aufgabe?retryWrites=true&w=majority";
+    let studentList;
+    let databaseUrl = "mongodb+srv://gisuser2020:dv1Y6ayeEBvevpAf@marcelgis.1jm82.mongodb.net/Test?retryWrites=true&w=majority";
+    //let databaseUrl: string = "mongodb://localhost: 27017";
     console.log("Starting server");
+    //Port Number wird unter port gespeichert
     let port = Number(process.env.PORT);
+    //Wenn port nicht erreichbar, wird Wert 8100 vergeben
     if (!port)
         port = 8100;
     connectToDatabase(databaseUrl);
+    //Server und Listener erstellen
     let server = Http.createServer();
     server.addListener("request", handleRequest);
     server.addListener("listening", handleListen);
@@ -21,24 +25,24 @@ var Aufgabe11;
         let options = { useNewUrlParser: true, useUnifiedTopology: true };
         let mongoClient = new Mongo.MongoClient(_url, options);
         await mongoClient.connect();
-        collection = mongoClient.db("Test").collection("Students");
-        console.log("Database connection ", collection != undefined);
+        studentList = mongoClient.db("Test").collection("Students");
+        console.log("Database connection ", studentList != undefined);
     }
     function handleListen() {
         console.log("Listening");
     }
+    //Server Daten erhalten
     async function handleRequest(_request, _response) {
         _response.setHeader("content-type", "text/html; charset=utf-8");
         _response.setHeader("Access-Control-Allow-Origin", "*");
         if (_request.url) {
             let url = Url.parse(_request.url, true);
             if (url.pathname == "/senden")
-                collection.insertOne(url.query);
+                studentList.insertOne(url.query);
             else if (url.pathname == "/holen") {
-                _response.write(JSON.stringify(await collection.find().toArray()));
+                _response.write(JSON.stringify(await studentList.find().toArray()));
             }
         }
         _response.end();
     }
 })(Aufgabe11 = exports.Aufgabe11 || (exports.Aufgabe11 = {}));
-//# sourceMappingURL=server.js.map
