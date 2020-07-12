55d83c5cd57e6a97116cf7833e70b43b4d8f6e9c
chanisch01 <64133300+chanisch01@users.noreply.github.com>
2020-07-12 12:50:54 +0200

    .

--- Test/index.js
+++ Test/index.js
@@ -2,23 +2,24 @@
 var Aufgabe11;
 (function (Aufgabe11) {
     let buttonAbsenden = document.getElementById("Absenden");
-    buttonAbsenden.addEventListener("click", buttonclickAbsenden);
+    buttonAbsenden.addEventListener("click", buttonclickabsenden);
     let buttonBekommen = document.getElementById("Bekommen");
-    buttonBekommen.addEventListener("click", buttonclickBekommen);
+    buttonBekommen.addEventListener("click", buttonclickbekommen);
     let server = document.getElementById("server");
-    async function buttonclickAbsenden() {
-        console.log("An Datenbank");
+    async function buttonclickabsenden() {
         let form = new FormData(document.forms[0]);
         let query = new URLSearchParams(form);
         let url = "https://csgis2020.herokuapp.com";
-        url += "/absenden?" + query.toString();
+        url = url + "/absenden" + "?" + query.toString();
         await fetch(url);
     }
-    async function buttonclickBekommen() {
-        let url = "https://csgis2020.herokuapp.com/pull";
+    async function buttonclickbekommen() {
+        let url = "https://csgis2020.herokuapp.com";
+        url = url + "/bekommen";
         let serverResponse = await fetch(url);
         let stringResponse = await serverResponse.text();
         server.innerHTML = stringResponse;
+        console.log("bekommen");
     }
 })(Aufgabe11 || (Aufgabe11 = {}));
 //# sourceMappingURL=index.js.map
--- Test/index.ts
+++ Test/index.ts
@@ -1,29 +1,28 @@
 namespace Aufgabe11 {
 
     let buttonAbsenden: HTMLButtonElement = <HTMLButtonElement>document.getElementById("Absenden");
-    buttonAbsenden.addEventListener("click", buttonclickAbsenden);
-   
+    buttonAbsenden.addEventListener("click", buttonclickabsenden);
+
     let buttonBekommen: HTMLButtonElement = <HTMLButtonElement>document.getElementById("Bekommen");
-    buttonBekommen.addEventListener("click", buttonclickBekommen);
-   
+    buttonBekommen.addEventListener("click", buttonclickbekommen);
+
     let server: HTMLElement = <HTMLElement>document.getElementById("server");
 
-    async function buttonclickAbsenden(): Promise<void> {
-        console.log("An Datenbank");
-       
+    async function buttonclickabsenden(): Promise<void> {
         let form: FormData = new FormData(document.forms[0]);
         let query: URLSearchParams = new URLSearchParams(<any>form);
         let url: string = "https://csgis2020.herokuapp.com";
-        url += "/absenden?" + query.toString();
-       
+        url = url + "/absenden" + "?" + query.toString();
         await fetch(url);
-        
+
     }
-    async function buttonclickBekommen(): Promise<void> {
-        let url: string = "https://csgis2020.herokuapp.com/pull";
-        
+    async function buttonclickbekommen(): Promise<void> {
+        let url: string = "https://csgis2020.herokuapp.com";
+        url = url + "/bekommen";
         let serverResponse: Response = await fetch(url);
         let stringResponse: string = await serverResponse.text();
         server.innerHTML = stringResponse;
+
+        console.log("bekommen");
     }
 }
